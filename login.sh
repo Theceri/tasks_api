cb69e5131cb2c78664a1cef58af70ee56d009b0a
Paul Theceri <paul.theceri@gmail.com>
2022-05-07 00:41:39 +0300

    change

--- login.sh
+++ login.sh
@@ -1,2 +1,3 @@
-ssh root@138.68.189.32 'git clone https://github.com/Theceri/tasks_api.git; cd tasks_api; git pull; docker-compose down; docker-compose up --build -d'
+# ssh root@138.68.189.32 'git clone https://github.com/Theceri/tasks_api.git; cd tasks_api; git pull; docker-compose down; docker-compose up --build -d'
 # ssh root@138.68.189.32 'cd tasks_api; docker-compose down;'
+ifconfig -a
