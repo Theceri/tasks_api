b808a7b3bf35348ad568856c97ca221b7b403234
Paul Theceri <paul.theceri@gmail.com>
2022-05-07 00:25:59 +0300

    building .travis.yml

--- login.sh
+++ login.sh
@@ -1,8 +1,2 @@
-
-
-USERNAME=root
-HOSTS="138.68.189.32"
-SCRIPT=" cd tasksAPIClass;git pull ;docker-compose down; docker-compose up --build -d"
-for HOSTNAME in ${HOSTS} ; do
-    ssh -l ${USERNAME} ${HOSTNAME} "${SCRIPT}"
-done
+ssh root@138.68.189.32 'git clone https://github.com/Theceri/tasks_api.git; cd tasks_api; git pull; docker-compose down; docker-compose up --build -d'
+# ssh root@138.68.189.32 'cd tasks_api; docker-compose down;'
